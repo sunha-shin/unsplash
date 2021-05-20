be687e5082b40292688bdaba0f4b92939e1941b8
sunha-shin <shinsunha11@gmail.com>
2021-05-20 10:34:52 -0400

    add route, add home, search pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import styled from 'styled-components';
+import {Route, Switch} from 'react-router-dom';
+import Home from "./views/components/pages/Home";
+import Search from "./views/components/pages/Search";
 
 const App = () => {
     return (
         <Container>
-            App
+            <Switch>
+                <Route exact path={'/'} component={Home}/>
+                <Route exact path={'/search/photos/:query'} component={Search}/>
+            </Switch>
         </Container>
     )
 }
