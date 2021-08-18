80e8998cab2f9ec99c8a82d1703c91fb88a488f3
sunha-shin <shinsunha11@gmail.com>
2021-08-18 00:11:29 -0400

    Modified TopicByIdContainer

--- src/redux/topics/redux.js
+++ src/redux/topics/redux.js
@@ -75,10 +75,6 @@ const reducer = (state = initialState, action) => {
         case Action.Types.SET_TOPIC_PHOTOS : {
             return {
                 ...state,
-                // photos: {
-                //     // ...state.photos,
-                //     ...action.data
-                // }
                 photos: [...action.data]
             }
         }
--- src/views/containers/TopicByIdContainer.js
+++ src/views/containers/TopicByIdContainer.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components';
 import {useDispatch} from "react-redux";
 import {Action as TopicsAction} from "../../redux/topics/redux";
@@ -7,17 +7,24 @@ import TopicDetail from "../components/Topics/TopicDetail";
 import {ContentContainer} from "../components/shared/Layout/Layout.Styled";
 import TopicPhotosContainer from "./TopicPhotosContainer";
 import {topicsActions} from "../../redux/actionCreators";
+import TopicPhotos from "../components/Topics/TopicPhotos";
 
 const TopicByIdContainer = ({match}) => {
     const slug = match.params.slug;
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         getTopicById();
     }, [slug])
 
+    useEffect(() => {
+        topicsActions.getTopicPhotos(slug, {page})
+    }, [page])
+
     const getTopicById = () => {
         topicsActions.getTopicById(slug);
     }
+
 
     return (
         <Container>
